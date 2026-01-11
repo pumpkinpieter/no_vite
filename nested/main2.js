660c19ba10f5f1b9e94fd562ed140912c3273c33
Pieter Vandenberge <pumpkinpieter@gmail.com>
2026-01-11 12:33:26 -0800

    Seeing if gui works.

--- nested/main2.js
+++ nested/main2.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const panel = new GUI( { width: 310 } );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
